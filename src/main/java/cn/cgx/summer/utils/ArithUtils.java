26d1638b79c2544d50ae4100af76414ad13c55bb
summer <409044250@qq.com>
2017-02-10 12:00:13 +0800

    测试

--- src/main/java/cn/cgx/summer/utils/ArithUtils.java
+++ src/main/java/cn/cgx/summer/utils/ArithUtils.java
@@ -27,6 +27,7 @@ public class ArithUtils
     {
         BigDecimal b1 = new BigDecimal(Double.toString(v1));
         BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        System.out.println();
         return b1.add(b2).doubleValue();
     }
 
